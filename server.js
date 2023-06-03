3e048ed18b7aee6fbacf5150c72380caf472af49
vitalij99 <vitalisklymko@gmail.com>
2023-06-03 16:00:03 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+    "mongodb+srv://vitalisklymko:jmC9BcU1GufBUCOe@cluster0.jrzm3tg.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+    .connect(DB_HOST)
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("Database connection successful");
+        });
+    })
+    .catch((err) => {
+        console.log(err.message);
+        process.exit(1);
+    });
