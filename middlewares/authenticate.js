9f2a6b3b679b1c7ab0ce9a8ae7bbd767f34bed42
vitalij99 <vitalisklymko@gmail.com>
2023-06-10 22:48:03 +0300

    fix empty authenticate

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -11,18 +11,18 @@ const authenticate = async (req, res, next) => {
 
     if (bearer !== "Bearer") {
         next(HttpError(401));
-    }
-
-    try {
-        const { id } = jwt.verify(token, SECRET_KEY);
-        const user = await User.findById(id);
-        if (!user || !user.token) {
+    } else {
+        try {
+            const { id } = jwt.verify(token, SECRET_KEY);
+            const user = await User.findById(id);
+            if (!user || !user.token) {
+                next(HttpError(401));
+            }
+            req.user = user;
+            next();
+        } catch (error) {
             next(HttpError(401));
         }
-        req.user = user;
-        next();
-    } catch (error) {
-        next(HttpError(401));
     }
 };
 
